2dfbb572bfc51048789a7d899cce3ab4e1a6c1f3
praneeth662 <praneethgupta135>
2023-01-30 23:33:27 +0530

    Basic JS

---
+++ dom.js
@@ -0,0 +1,6 @@
+//console.log(document);
+var headerTitle = document.getElementById("main-header");
+headerTitle.style.borderBottom = "solid 3px #000";
+var header = document.getElementById("heading2");
+header.style.fontWeight = "bold";
+header.style.color = "green";
